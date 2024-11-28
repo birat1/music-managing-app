082ccf806d6c789d283c00c603d97d13c05bd9ba
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-28 13:01:38 +0000

    Implemented Song, Album, Tracklist and User API Viewsets

--- django-app/label_music_manager/api_views.py
+++ django-app/label_music_manager/api_views.py
@@ -1,3 +1,21 @@
 # Use this file for your API viewsets only
 # E.g., from rest_framework import ...
+from rest_framework import viewsets, permissions
+from .models import Album, Song, AlbumTracklistItem, MusicManagerUser
+from .serializers import AlbumSerializer, SongSerializer, AlbumTracklistSerializer, MusicManagerUserSerializer
 
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class AlbumTracklistViewSet(viewsets.ModelViewSet):
+    queryset = AlbumTracklistItem.objects.all()
+    serializer_class = AlbumTracklistSerializer
+
+class MusicManagerUserViewSet(viewsets.ModelViewSet):
+    queryset = MusicManagerUser.objects.all()
+    serializer_class = MusicManagerUserSerializer
