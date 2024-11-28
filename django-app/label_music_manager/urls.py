16938a6123e6bc21053f80be8b888425676ee892
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-28 13:02:04 +0000

    Implemented Song, Album, Tracklist API routes

--- django-app/label_music_manager/urls.py
+++ django-app/label_music_manager/urls.py
@@ -1,5 +1,18 @@
 # Use this file to specify your subapp's routes
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import AlbumListView
+from .api_views import AlbumViewSet, SongViewSet, AlbumTracklistViewSet
+
+router = DefaultRouter()
+router.register(r'albums', AlbumViewSet, basename='albums')
+router.register(r'songs', SongViewSet, basename='songs')
+router.register(r'tracklist', AlbumTracklistViewSet, basename='tracklist')
 
 urlpatterns = [
+    # Templated views
+    path('', AlbumListView.as_view(), name='album_list'),
 
+    # API endpoints
+    path('api/', include(router.urls)),
 ]
