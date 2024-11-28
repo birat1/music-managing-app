4a9fce91500305122ae98cc83acba66b62ef8576
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-28 13:02:37 +0000

    Implemented AlbumListView

--- django-app/label_music_manager/views.py
+++ django-app/label_music_manager/views.py
@@ -1,1 +1,15 @@
 # Use this file for your templated views only
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Album, Song, AlbumTracklistItem, MusicManagerUser
+
+class AlbumListView(LoginRequiredMixin, generic.ListView):
+    model = Album
+
+    def get_queryset(self):
+        music_manager_user = MusicManagerUser.objects.get(user=self.request.user)
+
+        # If user is an artist, only show their albums
+        if music_manager_user.permissions == 'artist':
+            return Album.objects.filter(artist=music_manager_user.display_name)
+        return Album.objects.all()
