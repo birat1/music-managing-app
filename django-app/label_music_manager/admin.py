f60d38f4c62bba53a2e768b79d02179341aef620
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-30 13:22:05 +0000

    Models display on Admin Panel for easier access

--- django-app/label_music_manager/admin.py
+++ django-app/label_music_manager/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Album, Song, AlbumTracklistItem, MusicManagerUser
 
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(AlbumTracklistItem)
+admin.site.register(MusicManagerUser)
