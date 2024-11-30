cefa4f6c458937fdb3cb64b761e7f316650f9e16
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-30 21:13:49 +0000

    Implemented AlbumListView with authentication

--- django-app/label_music_manager/views.py
+++ django-app/label_music_manager/views.py
@@ -1,15 +1,18 @@
 # Use this file for your templated views only
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-from .models import Album, Song, AlbumTracklistItem, MusicManagerUser
+from .models import Album, MusicManagerUser
 
 class AlbumListView(LoginRequiredMixin, generic.ListView):
     model = Album
+    context_object_name = 'albums'
+    template_name = 'label_music_manager/album_list.html'
 
     def get_queryset(self):
-        music_manager_user = MusicManagerUser.objects.get(user=self.request.user)
+        user = self.request.user
+        music_manager_user = MusicManagerUser.objects.get(user=user)
 
-        # If user is an artist, only show their albums
-        if music_manager_user.permissions == 'artist':
-            return Album.objects.filter(artist=music_manager_user.display_name)
+        if user.is_authenticated and user.has_perm('label_music_manager.Artist'):
+            return Album.objects.filter(artist=music_manager_user.display_name) # Artists can only view their own albums
+        # Unauthenticated users, viewers, and editors can view all albums.
         return Album.objects.all()
