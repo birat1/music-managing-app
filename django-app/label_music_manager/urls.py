669f43fe04fe86f5fa24c6bd0d34708839fc0359
ba01081 <93601583+birat1@users.noreply.github.com>
2024-11-30 22:58:07 +0000

    Implemented AlbumDetailView

--- django-app/label_music_manager/urls.py
+++ django-app/label_music_manager/urls.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import AlbumListView
+from .views import AlbumListView, AlbumDetailView
 from .api_views import AlbumViewSet, SongViewSet, AlbumTracklistViewSet
 
 router = DefaultRouter()
@@ -13,6 +13,8 @@ router.register(r'tracklist', AlbumTracklistViewSet, basename='tracklist')
 urlpatterns = [
     # Templated views
     path('', AlbumListView.as_view(), name='album_list'),
+    path('albums/<int:id>/', AlbumDetailView.as_view(), name='album_detail_id'),
+    path('albums/<int:id>/<slug:slug>/', AlbumDetailView.as_view(), name='album_detail_id'),
 
     # API endpoints
     path('api/', include(router.urls)),
--- django-app/label_music_manager/views.py
+++ django-app/label_music_manager/views.py
@@ -1,9 +1,14 @@
 # Use this file for your templated views only
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views import generic
-from .models import Album, MusicManagerUser
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.generics import get_object_or_404
 
-class AlbumListView(LoginRequiredMixin, generic.ListView):
+from .models import Album, MusicManagerUser, AlbumTracklistItem
+
+
+class AlbumListView(LoginRequiredMixin, ListView):
     model = Album
     context_object_name = 'albums'
     template_name = 'label_music_manager/album_list.html'
@@ -16,3 +21,42 @@ class AlbumListView(LoginRequiredMixin, generic.ListView):
             return Album.objects.filter(artist=music_manager_user.display_name) # Artists can only view their own albums
         # Unauthenticated users, viewers, and editors can view all albums.
         return Album.objects.all()
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(**kwargs)
+
+        music_manager_user = MusicManagerUser.objects.get(user=user)
+        context['display_name'] = music_manager_user.display_name
+
+        return context
+
+class AlbumDetailView(LoginRequiredMixin, DetailView):
+    model = Album
+    context_object_name = 'album'
+    template_name = 'label_music_manager/album_detail.html'
+
+    def get_object(self, queryset=None):
+        album_id = self.kwargs.get('id')
+        album_slug = self.kwargs.get('slug')
+
+        if album_slug:
+            # Look up album by ID and slug
+            return get_object_or_404(Album, id=album_id, slug=album_slug)
+        # Look up album by ID only (for the /albums/:id format)
+        return get_object_or_404(Album, id=album_id)
+
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(**kwargs)
+
+        music_manager_user = MusicManagerUser.objects.get(user=user)
+        context['display_name'] = music_manager_user.display_name
+
+        # Fetch all tracks related to the album through the AlbumTracklistItem model
+        album = self.object
+        track_items = AlbumTracklistItem.objects.filter(album=album).order_by('position')
+        tracks = [item.song for item in track_items]
+        context['tracks'] = tracks
+
+        return context
