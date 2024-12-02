3b0b2b931ec768d3cb67be6c1e16e1e2a8efcdba
ba01081 <93601583+birat1@users.noreply.github.com>
2024-12-02 14:01:39 +0000

    Implemented navbar

---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+function AppNavbar() {
+  return (
+    <Navbar bg='dark' variant='dark' expand='lg' className='border-bottom'>
+      <Container fluid>
+        <Navbar.Brand href="/" className='fw-bold'>MyMusicMaestro</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default AppNavbar;
